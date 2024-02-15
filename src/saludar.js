c4cdcda228b75a92b741418e9de77a8d7b90b983
AndrePruden <andrepruden0@gmail.com>
2024-02-15 10:10:58 -0400

    Se anade funcionalidad de saludo por genero

--- src/saludar.js
+++ src/saludar.js
@@ -1,7 +1,11 @@
-function saludar(nombre, edad) {
-  if(edad > 30)
+function saludar(nombre, edad, genero) {
+  if(edad > 30 && genero === "masculino")
   {
     return "Sr " + nombre;
+  }
+  else if(edad > 30 && genero === "femenino")
+  {
+    return "Sra " + nombre;
   }
   else
     return nombre;
